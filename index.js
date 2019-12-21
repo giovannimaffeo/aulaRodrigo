8f21375be9a94ab8fb89077c175d50dc36e3f995
giovannimaffeo <giovanni.maffeo@gmail.com>
2019-12-20 21:56:18 -0300

    store configurada

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 /**
  * @format
  */
-
-import {AppRegistry} from 'react-native';
+import React from 'react'
+import {AppRegistry, StatusBar} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import store from './src/redux/store';
+import {Provider} from 'react-redux';
 
-AppRegistry.registerComponent(appName, () => App);
+const MyApp = () => (
+    <Provider store={store}>
+        <StatusBar hidden/>
+        <App />
+
+    </Provider>
+)
+AppRegistry.registerComponent(appName, () => MyApp);
